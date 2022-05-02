1f0f6926b0411ad9c3fab3836722d9b0b56456e1
Narcikss <mdgamedeveloper@gmail.com>
2022-05-02 11:19:05 +0200

    commit

--- site/src/scripts/card.js
+++ site/src/scripts/card.js
@@ -4,10 +4,10 @@ function cardTemplate(number, description, properlyAnswer) {
     this.properlyAnswer = properlyAnswer;
 }
 
-const card1 = new cardTemplate(0, "Is coca cola corporation big?", 'yes');
+const card1 = new cardTemplate(0, "Is coca cola a big corporation?", 'yes');
 const card2 = new cardTemplate(1, 'Did Elon Musk buy twitter?', 'yes');
 const card3 = new cardTemplate(2, 'Did Elon Musk buy twitter for 34 bilion dollars?', 'no');
-const card4 = new cardTemplate(3, 'Is your salary a joke?', 'yes');
+const card4 = new cardTemplate(3, 'Say yes', 'yes');
 
 const allCards = new Array();
 
